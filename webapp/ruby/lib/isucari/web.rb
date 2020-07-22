3df9e16f10d470c272f6f9b461ca7b4952477454
ohbarye <over.rye@gmail.com>
2020-07-22 23:14:37 +0900

    Revert "Do not query to categories since it is fixed data"
    This reverts commit 68a6acc2f554959b3ffd230be06de130add0fd08.

--- webapp/ruby/lib/isucari/web.rb
+++ webapp/ruby/lib/isucari/web.rb
@@ -92,7 +92,7 @@ module Isucari
       end
 
       def get_category_by_id(category_id)
-        category categories_hash[category_id]
+        category = db.xquery('SELECT * FROM `categories` WHERE `id` = ?', category_id).first
 
         return if category.nil?
 
@@ -138,32 +138,6 @@ module Isucari
 
       def halt_with_error(status = 500, error = 'unknown')
         halt status, { 'error' => error }.to_json
-      end
-
-      def categories
-        @@categories ||= begin
-          db.xquery('SELECT * FROM `categories`').to_a
-        end
-      end
-
-      def categories_hash
-        @@categories_hash ||= begin
-          h = {}
-          categories.each do |row|
-            h[row['id']] = row
-          end
-          h
-        end
-      end
-
-      def categories_hash_indexed_by_parent
-        @@categories_hash_indexed_by_parent ||= begin
-          h = {}
-          categories.each do |row|
-            h[row['parent_id']] = row
-          end
-          h
-        end
       end
     end
 
@@ -249,7 +223,7 @@ module Isucari
       root_category = get_category_by_id(root_category_id)
       halt_with_error 404, 'category not found' if root_category.nil?
 
-      category_ids = categories_hash_indexed_by_parent[root_category['id']].map { |row| row['id'] }
+      category_ids = db.xquery('SELECT id FROM `categories` WHERE parent_id = ?', root_category['id']).map { |row| row['id'] }
 
       item_id = params['item_id'].to_i
       created_at = params['created_at'].to_i
@@ -1165,6 +1139,7 @@ module Isucari
       response['user'] = user unless user.nil?
       response['payment_service_url'] = get_payment_service_url
 
+      categories = db.xquery('SELECT * FROM `categories`').to_a
       response['categories'] = categories
 
       response.to_json
